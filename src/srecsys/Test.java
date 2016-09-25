edd6f736f4c972160e014cff8b80e0afc9ee47dc
Andrey Simaputera <simaputera.andrey@gmail.com>
2016-09-26 04:16:24 +0700

    another minor before testing

--- src/srecsys/Test.java
+++ src/srecsys/Test.java
@@ -1,14 +1,11 @@
 package srecsys;
 
-import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
-import java.util.Scanner;
 import java.util.Set;
 import java.util.logging.Level;
 import java.util.logging.Logger;
